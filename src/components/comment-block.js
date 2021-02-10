afdbf30102cb9d393f444bf89d48b1906d2fd40c
Houstonka <47265607+Houstonka@users.noreply.github.com>
2021-02-10 13:09:19 +1000

    Add files via upload

--- src/components/comment-block.js
+++ src/components/comment-block.js
@@ -14,7 +14,7 @@ const comment = (params) => {
             <div  className='comment card  mb-3' key={comment.id}>
               <div className="card-body">
                 <h5 className="card-title" id='author'>{comment.author}</h5>
-                <p className="card-text" id='text' dangerouslySetInnerHTML={{__html: comment.text}}></p>
+                <p className="card-text" id='text'>{comment.text}</p>
                 <small>{comment.date} {comment.time}</small><br></br>
 
                 <button type="button" className="btn btn-danger" onClick = {ev => {deleteComment(comment.id)}}>Удалить</button>
